dde85172de53728752e8b07d12b754251c6caf3c
Diego Andres Benitez <47540258+andres-1994@users.noreply.github.com>
2020-08-03 12:10:14 -0400

    v3
    version nueva

---
+++ main.js
@@ -0,0 +1,16 @@
+const typed = new Typed('.typed', {
+
+	strings:['Es un placer', 'Es un placer'],
+
+	 typeSpeed: 70,
+	 startDelay: 300,
+	 backSpeed: 70,
+	 smartBackspace: false,
+	 shuffle: false,
+	 backDelay: 1500,
+	 loop: true,
+	 loopCount: false,
+	 showCursor: true,
+	 cursorChar: '|',
+	 contentType: 'html'
+});
